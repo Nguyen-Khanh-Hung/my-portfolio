431b3168cd893a42f8cb6e0cb89a5ebd111deec3
Nguyen Khanh Hung <hungnk19406c@st.uel.edu.vn>
2022-11-17 09:26:40 +0700

    update17/11/2022

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,14 +1,34 @@
 import "./intro.scss";
-
+import { useEffect, useRef } from "react";
+import { ArrowDownward } from "@material-ui/icons";
+import { init } from "ityped";
 export default function Intro() {
+  const job = useRef();
+  useEffect(() => {
+    init(job.current, {
+      showCursor: false,
+      strings: ["Intern Front-end Developer", "No pain, no gain"],
+    });
+  }, []);
   return (
     <div className="intro" id="intro">
       <div className="left">
         <div className="imgContainer">
-          <img src="assets/avt.png" alt="" />
+          <img src="assets\avt.png" alt="" />
         </div>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there, I'm</h2>
+          <h1>Nguyen Khanh Hung</h1>
+          <h3>
+            !! <span ref={job}></span>
+          </h3>
+          <a href="#portfolio">
+            <ArrowDownward className="iconDown" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -3,7 +3,19 @@ import "./portfolio.scss";
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-      Tui là Portfolio
+      <h1>Porfolio</h1>
+      <ul>
+        <li className="active">Landing page</li>
+        <li>Music app</li>
+        <li>Restaurant page</li>
+        <li>Ecommerce page</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Banking app</h3>
+        </div>
+      </div>
     </div>
   );
 }
